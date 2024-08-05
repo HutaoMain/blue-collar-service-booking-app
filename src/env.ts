bc5bb249e77c2a02028fec3f23b7a2b5089cf30c
HutaoMain <alimohamedalcantara@gmail.com>
2024-08-05 08:52:52 +0800

    85% customer

---
+++ src/env.ts
@@ -0,0 +1,6 @@
+export const apiKey = "AIzaSyCcUefinx4eKcosWUFWe768Tn8f5kaQ9bg";
+export const authDomain = "rn-fitar.firebaseapp.com";
+export const projectId = "rn-fitar";
+export const storageBucket = "rn-fitar.appspot.com";
+export const messagingSenderId = "822702412527";
+export const appId = "1:822702412527:web:623f72920cb3e3b5c6c11a";
